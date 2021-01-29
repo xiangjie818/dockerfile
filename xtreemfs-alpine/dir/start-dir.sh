0b1389d6040cb42267194617247a680a6a68e171
xiangjie818 <xiangjie818@163.com>
2021-01-29 02:21:31 +0000

    2020-01-29

--- xtreemfs-alpine/dir/start-dir.sh
+++ xtreemfs-alpine/dir/start-dir.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+chown -R root:xtreemfs /etc/xos/xtreemfs
+chown -R xtreemfs:root /var/lib/xtreemfs
 while true
 do
     su -s /bin/bash xtreemfs -c "/usr/bin/java -ea -cp /usr/share/java/xtreemfs.jar:/usr/share/java/jdmkrt.jar:/usr/share/java/jdmktk.jar:/usr/share/java/commons-codec-1.3.jar org.xtreemfs.dir.DIR  /etc/xos/xtreemfs/dirconfig.properties"
--- xtreemfs-alpine/mrc/start-mrc.sh
+++ xtreemfs-alpine/mrc/start-mrc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sed -i "s@dir_service.host =.*@dir_service.host = $DIR_SERVICE_HOST@g" /etc/xos/xtreemfs/mrcconfig.properties
 sed -i "s@# hostname = .*@hostname = ${HOSTNAME}@g" /etc/xos/xtreemfs/mrcconfig.properties
+chown -R root:xtreemfs /etc/xos/xtreemfs
+chown -R xtreemfs:root /var/lib/xtreemfs
+
 while true
 do
     su -s /bin/bash xtreemfs -c "/usr/bin/java -ea -cp /usr/share/java/xtreemfs.jar:/usr/share/java/jdmkrt.jar:/usr/share/java/jdmktk.jar:/usr/share/java/commons-codec-1.3.jar org.xtreemfs.mrc.MRC  /etc/xos/xtreemfs/mrcconfig.properties"
